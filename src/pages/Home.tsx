602275e7cd9057ec48afe7248ee3eb47c1ab7280
Camilo Jimenez <jucajigue+developer@gmail.com>
2025-01-21 11:29:56 -0500

    feat: add swiper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import HomeSlider from './components/HomeSlider';
 
 const Home: React.FC = () => {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold">Home Page</h1>
-      <p className="mt-4 text-lg">¡Bienvenido a la página principal!</p>
-    </div>
+    <>
+      <HomeSlider />
+    </>
   );
 };
 
