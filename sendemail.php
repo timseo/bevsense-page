51987c3999b755eb59a5651a41cb774248996f7b
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2025-01-31 20:24:46 +0600

    update

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "camm" );
-define( "RECIPIENT_EMAIL", "info@camm.org" );
+define( "RECIPIENT_NAME", "ali" );
+define( "RECIPIENT_EMAIL", "ali@collabola.com" );
 
 
 // Read the form values
